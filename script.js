caa049ab3c7d76d8ef478366967c59a07af3f083
Kye Otsuka <146736909+kye-otsuka@users.noreply.github.com>
2025-06-29 22:43:45 +0100

    First proper commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+console.log("JS is running")
