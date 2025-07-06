a19ff53aa31c126dbd4a631bc1182d0115c54724
Kye Otsuka <146736909+kye-otsuka@users.noreply.github.com>
2025-07-06 17:27:39 +0100

    Improved nav bar styling, hamburger menu functionality and text margin.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', (event) => {
   navLinks.classList.toggle('active');
+  event.stopPropagation();
 });
 
-console.log("JS is running")
+document.addEventListener('click', (event) => {
+  const isMenuOpen = navLinks.classList.contains('active');
+  const isClickOutside = !navLinks.contains(event.target);
+
+  if (isMenuOpen && isClickOutside) {
+    navLinks.classList.remove('active');
+  }
+});
+
+navLinks.addEventListener('click', () => {
+  navLinks.classList.remove('active');
+});
